02d49f1d36c6d4f362b3eb6425cdb0f23674de2c
Fuyao Zhao <zfy0701@gmail.com>
2023-07-22 14:30:19 -0700

    feat(aptos): initial view function

--- packages/aptos/src/codegen/codegen.ts
+++ packages/aptos/src/codegen/codegen.ts
@@ -3,7 +3,7 @@ import { Event, MoveModuleBytecode, MoveResource } from '../move-types.js'
 import chalk from 'chalk'
 import { join } from 'path'
 import { AptosChainAdapter } from '../aptos-chain-adapter.js'
-import { AbstractCodegen } from '@typemove/move'
+import { AbstractCodegen, camel, InternalMoveFunction, InternalMoveModule, normalizeToJSName } from '@typemove/move'
 
 export async function codegen(
   abisDir: string,
@@ -20,10 +20,7 @@ export async function codegen(
   console.log(chalk.green(`Generated ${numFiles} for Aptos`))
 }
 
-class AptosCodegen extends AbstractCodegen<
-  MoveModuleBytecode,
-  Event | MoveResource
-> {
+class AptosCodegen extends AbstractCodegen<MoveModuleBytecode, Event | MoveResource> {
   ADDRESS_TYPE = 'Address'
   REFERENCE_TYPE = 'Address'
   PREFIX = 'Aptos'
@@ -33,10 +30,56 @@ class AptosCodegen extends AbstractCodegen<
     super(new AptosChainAdapter(endpoint))
   }
 
-  // generateImports(): string {
-  //   return `
-  //     ${super.generateImports()}
-  //     import { Address } from '@typemove/aptos'
-  //   `
-  // }
+  generateImports(): string {
+    return `
+      ${super.generateImports()}
+      import { AptosClient } from 'aptos'
+    `
+  }
+  protected generateExtra(module: InternalMoveModule) {
+    // const funcs = module.exposedFunctions.map((f) =>
+    //     this.generateBuilderForFunction(module, f)
+    // )
+
+    const viewFuncs = module.exposedFunctions.map((f) => this.generateViewFunction(module, f))
+
+    // export namespace builder {
+    //   ${funcs.join('\n')}
+    // }
+
+    return `
+    export namespace view {
+      ${viewFuncs.join('\n')}
+    }
+    `
+  }
+
+  protected generateViewFunction(module: InternalMoveModule, func: InternalMoveFunction): string {
+    if (!func.isView) {
+      return ''
+    }
+    const genericString = this.generateFunctionTypeParameters(func)
+    const fields = this.chainAdapter.getMeaningfulFunctionParams(func.params).map((param) => {
+      return this.generateTypeForDescriptor(param, module.address)
+    })
+
+    const typeParamArg = func.typeParams
+      .map((v, idx) => {
+        return `TypeDescriptor<T${idx}> | string`
+      })
+      .join(',')
+
+    // const args = this.generateArgs(module, func)
+
+    return `export async function ${camel(normalizeToJSName(func.name))}${genericString}(
+    client: AptosClient,
+    request: {
+      type_arguments: [${func.typeParams.map((_) => 'string').join(', ')}], 
+      arguments: [${fields.join(',')}]},
+    version?: bigint) {
+      const data = { ...request, function: "${module.address}::${module.name}::${func.name}", }
+      const res = await client.view(data, version?.toString())
+      return res
+    }`
+  }
 }
