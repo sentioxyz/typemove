d919394bc9e2da5ad7cf609939d8d6cf4ab88f34
Fuyao Zhao <zfy0701@gmail.com>
2023-08-03 12:46:21 -0700

    chore(build): use node resolution

--- examples/sui/src/index.ts
+++ examples/sui/src/index.ts
@@ -11,17 +11,18 @@ clob_v2.builder.getMarketPrice(
   ['0x5d2687b354f2ad4bce90c828974346d91ac1787ff170e5d09cb769e5dbcdefae'],
   ['0x2::sui::SUI', '0x219d80b1be5d586ff3bdbfeaf4d051ec721442c3a6498a3222773c6945a73d9f::usdt::USDT']
 )
+console.log(tx.blockData)
 
-const result = await provider.devInspectTransactionBlock({
+const result = provider.devInspectTransactionBlock({
   transactionBlock: tx,
   sender: '0xd9e6dc1e7f0790c18acf96b629f0a236d56de2f96537d921197bcb0e071b12bd',
 })
 
-console.log(tx.blockData)
+result.then(r => console.log(r))
 
-const balance = await _0x2.coin.view.balance(
+const balance = _0x2.coin.view.balance(
   provider,
   ['0x5d2687b354f2ad4bce90c828974346d91ac1787ff170e5d09cb769e5dbcdefae'],
   ['0x219d80b1be5d586ff3bdbfeaf4d051ec721442c3a6498a3222773c6945a73d9f::usdt::USDT']
 )
-console.log(balance)
+balance.then(b => console.log(b))
