7aad82417a5c4d1104247dc6933bfc45afed0ea7
philz3906 <philz3906@gmail.com>
2023-08-04 15:09:38 -0700

    chore(aptos): add more tests, add tests, fix encoding (#29)

---
+++ packages/aptos/src/tests/move-call.test.ts
@@ -0,0 +1,52 @@
+import {
+  base_pool,
+  fees,
+  stable_pool,
+} from './types/0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af'
+import { AptosClient } from 'aptos'
+import { _0x1 } from '@typemove/aptos/builtin'
+import { expect } from 'chai'
+
+describe('move-call', () => {
+  const aptosClient = new AptosClient('https://fullnode.mainnet.aptoslabs.com')
+
+  test('system-call', async () => {
+    const [res] = await _0x1.account.view.existsAt(aptosClient, {
+      type_arguments: [],
+      arguments: ['0x5967ebb35647e8a664ea8d2d96276f28cc88e7bfeff46e625c8900d8b541506a'],
+    })
+    expect(res).to.equal(true)
+
+    const [chainId] = await _0x1.chain_id.view.get(aptosClient, { type_arguments: [], arguments: [] })
+    expect(chainId).to.equal(1)
+
+    const [decimal] = await _0x1.coin.view.decimals(aptosClient, {
+      type_arguments: ['0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T'],
+      arguments: [],
+    })
+    expect(decimal).to.equal(6)
+  })
+
+  test('custom-call', async () => {
+    const [poolId] = await stable_pool.view.nextPoolId(aptosClient, { type_arguments: [], arguments: [] })
+    console.log(poolId)
+
+    const [balance] = await fees.view.balance(aptosClient, {
+      type_arguments: ['0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T'],
+      arguments: [],
+    })
+    console.log(balance)
+
+    const [decimal] = await base_pool.view.maxSupportedDecimals(aptosClient, { type_arguments: [], arguments: [] })
+    expect(decimal).to.equal(8)
+
+    const [lpName] = await stable_pool.view.lpNameById(aptosClient, { type_arguments: [], arguments: [3n] })
+    const [poolBalances, weights, supply] = await stable_pool.view.poolInfo(aptosClient, {
+      type_arguments: [],
+      arguments: [lpName],
+    })
+    expect(poolBalances.length > 0).equal(true)
+    expect(weights > 0n).equal(true)
+    expect(supply > 0n).equal(true)
+  })
+})
