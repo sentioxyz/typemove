047cceb62f98164d9da29d67f12a198bf9235ba9
philz3906 <philz3906@gmail.com>
2023-08-14 11:14:02 -0700

    chore(deps): upgrade deps (#38)

--- packages/sui/src/index.ts
+++ packages/sui/src/index.ts
@@ -1,6 +1,3 @@
 export * from './models.js'
 
-export type { SuiAddress, ObjectId } from '@mysten/sui.js'
-// export * as builtin from './builtin/index.js'
-
 export { MoveCoder, defaultMoveCoder } from './move-coder.js'
