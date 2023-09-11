ef98ba297b9eac4197450f2f8391ce8d9f9d6500
philz3906 <philz3906@gmail.com>
2023-09-11 14:13:28 -0700

    chore(deps,docs): update deps and update docs (#40)

--- packages/aptos/src/client.test.ts
+++ packages/aptos/src/client.test.ts
@@ -10,10 +10,10 @@ describe('client call of entry or view', () => {
   test('call balance', async () => {
     const data: Types.ViewRequest = {
       type_arguments: [
-        '0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin',
+        '0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin'
       ],
       arguments: ['0x5967ebb35647e8a664ea8d2d96276f28cc88e7bfeff46e625c8900d8b541506a'],
-      function: '0x1::coin::balance',
+      function: '0x1::coin::balance'
     }
     const res1 = await client.view(data, '193435152')
     expect(res1).eql(['99680593'])
@@ -22,9 +22,9 @@ describe('client call of entry or view', () => {
       client,
       {
         type_arguments: [
-          '0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin',
+          '0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin'
         ],
-        arguments: ['0x5967ebb35647e8a664ea8d2d96276f28cc88e7bfeff46e625c8900d8b541506a'],
+        arguments: ['0x5967ebb35647e8a664ea8d2d96276f28cc88e7bfeff46e625c8900d8b541506a']
       },
       193435152n
     )
@@ -33,7 +33,7 @@ describe('client call of entry or view', () => {
 
   test('call get_validator_config', async () => {
     const res = await _0x1.stake.view.getValidatorConfig(client, {
-      arguments: ['0xee49776eff9fd395eb90d601449542080645e63704f518b31c6f72b6a95d7868'],
+      arguments: ['0xee49776eff9fd395eb90d601449542080645e63704f518b31c6f72b6a95d7868']
     })
     expect(res.length).eql(3)
   })
@@ -42,7 +42,7 @@ describe('client call of entry or view', () => {
     const account = new AptosAccount(undefined, '0x5967ebb35647e8a664ea8d2d96276f28cc88e7bfeff46e625c8900d8b541506a')
     const res = await _0x1.coin.entry.transfer(client, account, {
       type_arguments: ['0x1::aptos_coin::AptosCoin'],
-      arguments: ['0x1', 1n],
+      arguments: ['0x1', 1n]
     })
   })
 })
