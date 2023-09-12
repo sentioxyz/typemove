c3f2dd3998f079fac5d59f841a0569195304cd52
philz3906 <philz3906@gmail.com>
2023-09-11 17:05:25 -0700

    feat(aptos): add resource client (#41)

--- packages/aptos/src/index.ts
+++ packages/aptos/src/index.ts
@@ -1,3 +1,4 @@
 export * from './models.js'
 export * from './move-types.js'
 export { MoveCoder, defaultMoveCoder } from './move-coder.js'
+export { AccountResourceClient } from './account-resource-client.js'
