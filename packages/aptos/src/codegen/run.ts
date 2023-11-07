415c06a71b3fa45033fc7f2245e2cd335d112510
Fuyao Zhao <zfy0701@gmail.com>
2023-11-07 22:52:53 +0800

    chore(aptos): make code gen cli more reliable with commander.js

--- packages/aptos/src/codegen/run.ts
+++ packages/aptos/src/codegen/run.ts
@@ -4,30 +4,49 @@ import { codegen } from './codegen.js'
 import { AptosClient } from 'aptos'
 import * as path from 'path'
 import * as fs from 'fs'
+import { Command } from 'commander'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const pkg = require('../../package.json')
 
-if (process.argv.length > 3) {
-  let abisDir = process.argv[2]
-  const targetDir = process.argv[3]
-  let endpoint: string | undefined = process.argv[4]
-  if (!endpoint || endpoint == 'mainnet') {
-    endpoint = 'https://mainnet.aptoslabs.com/'
-  }
-  if (endpoint == 'testnet') {
-    endpoint = 'https://testnet.aptoslabs.com/'
-  }
-  const aptosClient = new AptosClient(endpoint)
-  if (abisDir.startsWith('0x')) {
-    const abiAddress = abisDir
-    const abi = await aptosClient.getAccountModules(abiAddress)
-    abisDir = path.join(targetDir, '..', 'abis')
-    if (!fs.existsSync(abisDir)) {
-      fs.mkdirSync(abisDir, { recursive: true })
+const program = new Command()
+
+program
+  .name(pkg.name)
+  .description('CLI to generate typescript types from aptos ABIs')
+  .showHelpAfterError()
+  .version(pkg.version)
+  .argument('<location>', 'Directory of ABI json files or address of account to generate types for')
+  .option('-a, --abi-dir <dir>', 'Directory to store downloaded ABI. Only useful if <location> is address', './abis')
+  .option('-t, --target-dir <dir>', 'Directory to output generated files', './types')
+  .option(
+    '-n, --network <mainnet|testnet|$url>',
+    'Aptos network to use, could be either "mainnet", "testnet" or any aptos node endpoint url',
+    'mainnet'
+  )
+  .action(async (location, options) => {
+    let endpoint = options.network
+    if (endpoint == 'mainnet') {
+      endpoint = 'https://mainnet.aptoslabs.com/'
     }
-    fs.writeFileSync(path.join(abisDir, abiAddress + '.json'), JSON.stringify(abi, null, 2))
-  }
+    if (endpoint == 'testnet') {
+      endpoint = 'https://testnet.aptoslabs.com/'
+    }
+    const aptosClient = new AptosClient(endpoint)
 
-  await codegen(abisDir, targetDir, endpoint, true)
-} else {
-  console.error('Not enough argument')
-  process.exit(1)
-}
+    let abisDir = location
+    if (location.startsWith('0x')) {
+      const abiAddress = abisDir
+      const abi = await aptosClient.getAccountModules(abiAddress)
+      abisDir = options.abiDir
+      if (!fs.existsSync(abisDir)) {
+        fs.mkdirSync(abisDir, { recursive: true })
+      }
+      fs.writeFileSync(path.join(abisDir, abiAddress + '.json'), JSON.stringify(abi, null, 2))
+    }
+
+    const num = await codegen(abisDir, options.targetDir, endpoint, true)
+    process.exit(num == 0 ? 1 : 0)
+  })
+
+program.parse()
