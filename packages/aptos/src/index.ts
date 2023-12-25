28cda04eb7e9c016e09ca8100a3947ab32a273df
philz3906 <philz3906@gmail.com>
2023-12-25 22:21:35 +0800

    chore(aptos): remove unecessary type (#66)

--- packages/aptos/src/index.ts
+++ packages/aptos/src/index.ts
@@ -1,4 +1,3 @@
 export * from './models.js'
-export * from './move-types.js'
 export { MoveCoder, defaultMoveCoder } from './move-coder.js'
 export { AccountResourceClient } from './account-resource-client.js'
