0d22db60b6c066a33c4077491b7f7e3369c3d07b
Ping Chen <remotenonsense@gmail.com>
2024-07-11 15:48:43 +0900

    chore(deps): replace jest with node test runner (#103)

--- packages/aptos/jest.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest/presets/default-esm',
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-    '^@typemove/aptos(.*)$': '<rootDir>/src/$1',
-  },
-}
