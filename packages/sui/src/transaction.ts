f69b1b6ba678298dc1ddb154c3ae04768c6bdfed
Fuyao Zhao <zfy0701@gmail.com>
2024-11-09 10:54:19 +0800

    fix(sui): better input handles on different types

--- packages/sui/src/transaction.ts
+++ packages/sui/src/transaction.ts
@@ -1,4 +1,4 @@
-import { Transaction } from '@mysten/sui/transactions'
+import { Transaction, TransactionArgument, TransactionObjectInput } from '@mysten/sui/transactions'
 
 export function isTransactionArgument(value: any): boolean {
   if (typeof value !== 'object') return false
@@ -7,9 +7,12 @@ export function isTransactionArgument(value: any): boolean {
   return value.kind === 'GasCoin' || value.kind === 'Result' || value.kind === 'NestedResult' || value.kind === 'Input'
 }
 
-export function transactionArgumentOrObject(value: any, transactionBlock: Transaction): any {
+export function transactionArgumentOrObject(
+  value: TransactionArgument | TransactionObjectInput,
+  transactionBlock: Transaction
+): TransactionArgument {
   if (isTransactionArgument(value)) {
-    return value
+    return value as TransactionArgument
   }
   return transactionBlock.object(value)
 }
@@ -21,6 +24,97 @@ export function transactionArgumentOrPure(value: any, transactionBlock: Transact
   return typeof value == 'string' ? transactionBlock.pure.string(value) : transactionBlock.pure.u64(value)
 }
 
+export function transactionArgumentOrPureString(
+  value: TransactionArgument | string,
+  transactionBlock: Transaction
+): TransactionArgument {
+  if (isTransactionArgument(value)) {
+    return value as TransactionArgument
+  }
+  return transactionBlock.pure.string(value as string)
+}
+
+export function transactionArgumentOrPureAddress(
+  value: TransactionArgument | string,
+  transactionBlock: Transaction
+): TransactionArgument {
+  if (isTransactionArgument(value)) {
+    return value as TransactionArgument
+  }
+  return transactionBlock.pure.address(value as string)
+}
+
+export function transactionArgumentOrPureU8(
+  value: TransactionArgument | number,
+  transactionBlock: Transaction
+): TransactionArgument {
+  if (isTransactionArgument(value)) {
+    return value as TransactionArgument
+  }
+  return transactionBlock.pure.u8(value as number)
+}
+
+export function transactionArgumentOrPureU16(
+  value: TransactionArgument | number,
+  transactionBlock: Transaction
+): TransactionArgument {
+  if (isTransactionArgument(value)) {
+    return value as TransactionArgument
+  }
+  return transactionBlock.pure.u16(value as number)
+}
+
+export function transactionArgumentOrPureU32(
+  value: TransactionArgument | number,
+  transactionBlock: Transaction
+): TransactionArgument {
+  if (isTransactionArgument(value)) {
+    return value as TransactionArgument
+  }
+  return transactionBlock.pure.u32(value as number)
+}
+
+export function transactionArgumentOrPureU64(
+  value: TransactionArgument | bigint | number | string,
+  transactionBlock: Transaction
+): TransactionArgument {
+  if (isTransactionArgument(value)) {
+    return value as TransactionArgument
+  }
+  return transactionBlock.pure.u64(value as number)
+}
+
+export function transactionArgumentOrPureU128(
+  value: TransactionArgument | bigint | number | string,
+  transactionBlock: Transaction
+): TransactionArgument {
+  if (isTransactionArgument(value)) {
+    return value as TransactionArgument
+  }
+  return transactionBlock.pure.u128(value as number)
+}
+
+export function transactionArgumentOrPureU256(
+  value: TransactionArgument | bigint | number | string,
+  transactionBlock: Transaction
+): TransactionArgument {
+  if (isTransactionArgument(value)) {
+    return value as TransactionArgument
+  }
+  return transactionBlock.pure.u256(value as number)
+}
+
+export function transactionArgumentOrPureBool(
+  value: TransactionArgument | boolean,
+  transactionBlock: Transaction
+): TransactionArgument {
+  if (isTransactionArgument(value)) {
+    return value as TransactionArgument
+  }
+  return transactionBlock.pure.bool(value as boolean)
+}
+
+// TODO vector might be nested
 export function transactionArgumentOrVec(value: any, transactionBlock: Transaction): any {
   if (isTransactionArgument(value)) {
     return value
